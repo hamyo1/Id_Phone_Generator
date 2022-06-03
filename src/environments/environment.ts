0b9c4f07e5a8f2bc8e279421a1a5db3749fbb5b8
hamyo1 <48315227+hamyo1@users.noreply.github.com>
2022-06-03 16:17:28 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,6 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyAgg0KJnXQQOb9XniGvMQ7dWpx5SM5vthk",
-    authDomain: "idgeneratordb.firebaseapp.com",
-    databaseURL: "https://idgeneratordb-default-rtdb.firebaseio.com",
-    projectId: "idgeneratordb",
-    storageBucket: "idgeneratordb.appspot.com",
-    messagingSenderId: "728151242040",
-    appId: "1:728151242040:web:1c4189a3a87311afc67dcd",
-    measurementId: "G-D3MMPMDFFG"
-  }
 };
 
 /*
